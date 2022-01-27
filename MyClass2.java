da41883a0e7c12631a602877abee65377bf61b6b
Chaithusane <98514558+Chaithusane@users.noreply.github.com>
2022-01-27 14:37:33 +0530

    Add files via upload

---
+++ MyClass2.java
@@ -0,0 +1,180 @@
+
+public class MyClass2 {
+
+	    public static void main(String args[]) 
+	    {
+	        int[] array = new int[3];
+	        try 
+	        {
+	            array[7] = 3;
+	        }
+	        catch (ArrayIndexOutOfBoundsException e) 
+	        {
+	            System.out.println("Array index is out of bounds!"); 
+	        }
+	        finally 
+	        {
+	            System.out.println("The array is of size " + array.length);
+	        }
+	    }
+	}
+	:THROWS                                                                                                                                                            public class MyClass 
+	{
+	    public static void main(String args[]) 
+	    {
+	        int[] array = new int[3];
+	        try 
+	        {
+	            array[7] = 3;
+	        }
+	        catch (ArrayIndexOutOfBoundsException e) 
+	        {
+	            System.out.println("Array index is out of bounds!"); 
+	        }
+	        finally 
+	        {
+	            System.out.println("The array is of size " + array.length);
+	        }
+	    }
+	}
+	 :FINALLY
+	public class MyClass 
+	{
+	    public static void main(String args[]) 
+	    {
+	        int[] array = new int[3];
+	        try 
+	        {
+	            array[7] = 3;
+	        }
+	        catch (ArrayIndexOutOfBoundsException e) 
+	        {
+	            System.out.println("Array index is out of bounds!"); 
+	        }
+	        finally 
+	        {
+	            System.out.println("The array is of size " + array.length);
+	        }
+	    }
+	}
+	:CUSTOM:
+	public class MyClass 
+	{
+	    public static void main(String args[]) 
+	    {
+	        int[] array = new int[3];
+	        try 
+	        {
+	            array[7] = 3;
+	        }
+	        catch (ArrayIndexOutOfBoundsException e) 
+	        {
+	            System.out.println("Array index is out of bounds!"); 
+	        }
+	        finally 
+	        {
+	            System.out.println("The array is of size " + array.length);
+	        }
+	    }
+	}
+	17:EXCEPTION
+	public class MyClass 
+	{
+	    public static void main(String args[]) 
+	    {
+	        int[] array = new int[3];
+	        try 
+	        {
+	            array[7] = 3;
+	        }
+	        catch (ArrayIndexOutOfBoundsException e) 
+	        {
+	            System.out.println("Array index is out of bounds!"); 
+	        }
+	        finally 
+	        {
+	            System.out.println("The array is of size " + array.length);
+	        }
+	    }
+	}
+	18:CREATE
+	public class MyClass 
+	{
+	    public static void main(String args[]) 
+	    {
+	        int[] array = new int[3];
+	        try 
+	        {
+	            array[7] = 3;
+	        }
+	        catch (ArrayIndexOutOfBoundsException e) 
+	        {
+	            System.out.println("Array index is out of bounds!"); 
+	        }
+	        finally 
+	        {
+	            System.out.println("The array is of size " + array.length);
+	        }
+	    }
+	}
+	:READ:
+	public class MyClass 
+	{
+	    public static void main(String args[]) 
+	    {
+	        int[] array = new int[3];
+	        try 
+	        {
+	            array[7] = 3;
+	        }
+	        catch (ArrayIndexOutOfBoundsException e) 
+	        {
+	            System.out.println("Array index is out of bounds!"); 
+	        }
+	        finally 
+	        {
+	            System.out.println("The array is of size " + array.length);
+	        }
+	    }
+	}
+	:UPDATE:
+	public class MyClass 
+	{
+	    public static void main(String args[]) 
+	    {
+	        int[] array = new int[3];
+	        try 
+	        {
+	            array[7] = 3;
+	        }
+	        catch (ArrayIndexOutOfBoundsException e) 
+	        {
+	            System.out.println("Array index is out of bounds!"); 
+	        }
+	        finally 
+	        {
+	            System.out.println("The array is of size " + array.length);
+	        }
+	    }
+	}
+	:DELETE:
+	public class MyClass 
+	{
+	    public static void main(String args[]) 
+	    {
+	        int[] array = new int[3];
+	        try 
+	        {
+	            array[7] = 3;
+	        }
+	        catch (ArrayIndexOutOfBoundsException e) 
+	        {
+	            System.out.println("Array index is out of bounds!"); 
+	        }
+	        finally 
+	        {
+	            System.out.println("The array is of size " + array.length);
+	        }
+	    }
+	}
+
